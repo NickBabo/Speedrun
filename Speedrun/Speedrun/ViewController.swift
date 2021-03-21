ae00744045e11bec2152ff803b6a7a9e93181206
Nicholas Babo <nickbabo7@gmail.com>
2021-03-21 19:51:55 -0300

    Add SpeedrunSDK dependency

---
+++ Speedrun/Speedrun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Speedrun
+//
+//  Created by Nick Babo on 21/03/21.
+//
+
+import UIKit
+import SpeedrunSDK
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
